06ba1bb61238d537a0c7a4bf5a2d7bb7d4208ce8
Bek <Bekvevo@gmail.com>
2018-08-24 15:07:01 +0500

    Blurry text bug detected

---
+++ UserControlSample/NoEffectHeadereContentControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlSample
+{
+    /// <summary>
+    /// Interaction logic for NoEffectHeadereContentControl.xaml
+    /// </summary>
+    public partial class NoEffectHeadereContentControl : UserControl
+    {
+        public NoEffectHeadereContentControl()
+        {
+            InitializeComponent();
+        }
+        public static readonly DependencyProperty ChildContentProperty =
+            DependencyProperty.Register("ChildContent", typeof(UIElement), typeof(NoEffectHeadereContentControl), new PropertyMetadata(null));
+
+        public UIElement ChildContent
+        {
+            get { return (UIElement)GetValue(ChildContentProperty); }
+            set { SetValue(ChildContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(NoEffectHeadereContentControl));
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+    }
+}
